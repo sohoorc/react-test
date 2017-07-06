e5b97d8da0cfd0fd8456fe9a31041b8fc8f13432
sohoorc <me@gwtcoder.me>
2017-07-06 13:55:12 +0800

    demo

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+var footerCss = require("../../css/footer.css");
+
 export default class ComponentFooter extends React.Component{
     render(){
+        console.log(footerCss)
+
         return (
-            <footer>
-                <h1>这里是页脚,一般放置版权信息.</h1>
-            </footer>
+            <footer className={footerCss.miniFooter}>
+                <h1>这里是页脚,一般放置版权信息.© </h1>
+            </footer> 
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,15 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs'], //添加组件的插件配置
         }
-      }
+      },
+      //下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
     ]
   },
   output: {

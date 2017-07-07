abab2a6cd0661033647b4de351eb36aae04bac31
sohoorc <me@gwtcoder.me>
2017-07-07 16:40:05 +0800

    add files

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,62 +1,88 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 // 导航
-import { Menu, Icon } from 'antd';
+import { Menu, Icon, Tabs, message, Form, Input, Button, CheckBox } from 'antd';
+
+const FormItem = Form.item;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
-
-
-export default class PCHeader extends React.Component{
-    constructor(){
+class PCHeader extends React.Component {
+    constructor() {
         super();
         this.state = {
-            current:'top'
+            current: 'top',
+            modalVisible:false,
+            action:'login',
+            hasLogined:false,
+            userNickName:'',
+            userid:0
         }
     }
 
-    render(){
-        return(
+    render() {
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined
+        ?
+        <Menu.Item key = "logout" class="register">
+            <Button type="primary" htmlType="button">{this.state.userNiceName}</Button>
+            &nbsp;$nbsp;
+            <Link target="_blank">
+                <Button type="dashed" htmlType="button">个人中心</Button>
+            </Link>
+            &nbsp;$nbsp;
+            <Button type="dashed" htmlType="button">个人中心</Button>
+        </Menu.Item>
+        :
+        <Menu.Item key="register" class="register">
+            <Icon type="appstore">注册/登录</Icon>
+        </Menu.Item>
+        ;
+
+        return (
             <header>
-               <Row>
-                   <Col span={2}></Col>
-                   <Col span={4}>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={4}>
                         <a href="/" className="logo">
-                            <img src="./src/images/logo.png" alt="logo"/> 
+                            <img src="./src/images/logo.png" alt="logo" />
                             <span>ReactNews</span>
                         </a>
-                   </Col>
-                   <Col span={16}>
+                    </Col>
+                    <Col span={16}>
                         <Menu mode="horizontal" selectedKeys={[this.state.current]}>
                             <Menu.Item key="top">
-                                <Icon type="appstore"/>头条
+                                <Icon type="appstore" />头条
                             </Menu.Item>
                             <Menu.Item key="shehui">
-                                <Icon type="appstore"/>社会
+                                <Icon type="appstore" />社会
                             </Menu.Item>
                             <Menu.Item key="guonei">
-                                <Icon type="appstore"/>国内
+                                <Icon type="appstore" />国内
                             </Menu.Item>
                             <Menu.Item key="guoji">
-                                <Icon type="appstore"/>国际
+                                <Icon type="appstore" />国际
                             </Menu.Item>
                             <Menu.Item key="yule">
-                                <Icon type="appstore"/>娱乐
+                                <Icon type="appstore" />娱乐
                             </Menu.Item>
                             <Menu.Item key="keji">
-                                <Icon type="appstore"/>科技
+                                <Icon type="appstore" />科技
                             </Menu.Item>
                             <Menu.Item key="tiyu">
-                                <Icon type="appstore"/>体育
+                                <Icon type="appstore" />体育
                             </Menu.Item>
                             <Menu.Item key="shishang">
-                                <Icon type="appstore"/>时尚
+                                <Icon type="appstore" />时尚
                             </Menu.Item>
+                            {userShow}
                         </Menu>
-                   </Col>
-                   <Col span={2}></Col>
-                </Row>  
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
             </header>
         );
     };
 }
+
+export default PCHeader = Form.create({})(PCHeader);

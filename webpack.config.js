b5d4223a392e3c6ffa3e64c2cacb991009b8e7e8
Weitian Gan <me@gwtcoder.me>
2018-05-16 18:46:26 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
           plugins: ['react-html-attrs'], //添加组件的插件配置
         }
       },
-      //下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
